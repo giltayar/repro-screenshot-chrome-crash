c0023802ee4fecbee2a426d47068e97a62c1d56c
Gil Tayar <gil@tayar.org>
2018-10-10 14:32:19 +0300

    more changes

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 'use strict'
+const {promisify} = require('util')
 const puppeteer = require('puppeteer')
 const fetch = require('node-fetch')
 
 async function main() {
+  console.log('connecting...')
   // const browser = await puppeteer.launch({headless: true})
-  console.log('connecting')
   const browser = await puppeteer.connect({browserWSEndpoint: await getBrowserWSEndpointId()})
+  const browserWidth = 2500
+  const browserHeight = 700
+  const pageHeight = 9891
+  const sectionHeight = 500
+  const sleepBetweenSectionScreenshots = 1000
 
   const page = await browser.newPage()
-  await page.setViewport({width: 2500, height: 700})
-  console.log('navigating to page')
+  await page.setViewport({width: browserWidth, height: browserHeight})
+
+  console.log('navigating to page...')
   await page.goto('https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop')
 
-  console.log('taking screenshot...')
-  const sectionHeight = 5000
-  for (let i = 0; i < 2; ++i) {
+  for (let i = 0; i < Math.ceil(pageHeight / sectionHeight); ++i) {
+    console.log(`taking screenshot #${i + 1}...`)
     await page.screenshot({
-      path: `screenshot-${i + 1}.png`,
+      path: `screenshots/screenshot-${i + 1}.png`,
       clip: {
         x: 0,
-        y: sectionHeight * i, width: 1018,
-        height: i === 0 ? sectionHeight : 9891 - sectionHeight
+        y: sectionHeight * i,
+        width: browserWidth,
+        height: i === 0 ? sectionHeight : pageHeight - sectionHeight
       }
     })
+    console.log('took screenshot')
+    if (sleepBetweenSectionScreenshots) await promisify(setTimeout)(sleepBetweenSectionScreenshots)
   }
-  console.log('took screenshot')
 }
 
 main().then(_ => process.exit(0)).catch(console.error)
